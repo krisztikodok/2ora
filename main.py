6e0f88beb21fa057a4cd9a9ec515e0b4e396f9e4
krisztikodok <135458893+krisztikodok@users.noreply.github.com>
2023-09-19 08:18:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,77 @@
+x=10
+nev="Béla"
+nev_empty=""
+print('Hello '+nev)
+str(10)
+print(x)
+x="Uj"
+print(x,type(x))
+x=3.14E-2
+z=6.66
+print(x,type(x))
+x=1+4j
+print(x,type(x))
+num=1/3
+#print("num=(0)".format(num))
+#print('num=(0.2f)'.format(num))
+
+h=2+4j
+k=5+1j
+l=6+7j
+com1=h*k
+print(com1)
+print("alma",end="")
+print("dinnye")
+print("eper")
+
+"""
+#var_a=input()
+for i in range(0,11,2):
+    print(i)
+
+for i in range(10,-1,-1):
+    print(i)
+
+i=0
+while i<=10:
+    print(i)
+    i=i+1
+
+i=0
+while True:
+    print(i)
+    if i==1:
+        break #to not be inf.loop
+    i=i+1
+
+"""
+x=range(5,10)
+for i in x:
+    print(i)
+
+
+li=[1,2,3,4,5]
+for j in li:
+    print(j)
+
+nev=["Anna","Bela",1,3,True]
+for i in nev:
+    print(i)
+
+a=input("Egész szám:")
+
+if a%2==0:
+    print('{} páros'.format(a))
+else:
+    print('{}nem páros'.format(a))
+
+
+
+
+
+
+
+
+
+
+
